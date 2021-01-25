17ffe939b208930bb5e91d3c5f5e5f0f18acdc17
DK <klimovdgprod@gmail.com>
2021-01-25 15:53:18 +0300

    FirstLession- Add Models Views Controller - return Model oject to view(List Of Movies)

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movie = new Movie { id = 1, Name = "Shrek" };
+            var movie2 = new Movie { id = 2, Name = "Titnaic" };
+            var movie3 = new Movie { id = 3, Name = "8 mile" };
+            var movie4 = new Movie { id = 4, Name = "La la land" };
+
+
+            //List<Movie> movies = new List<Movie> { movie, movie2, movie3, movie4 };
+
+            //ViewBag.Movies = movies;
+            var movies = new List<Movie> { movie, movie2, movie3, movie4 };
+
+            var ListMovies = new MoviesViewModel { MoviesList = movies };
+
+            return View(ListMovies);
+        }
+
+
+        public ActionResult Random()
+        {
+            return View();
+        }
+    }
+}
